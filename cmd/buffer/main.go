6eef0dcfa7a058fe6674d343c0a5457994fb860e
James Whitehead II <jnwhiteh@gmail.com>
2011-01-17 18:08:09 +0000

    Show how local import could be done

--- cmd/buffer/main.go
+++ cmd/buffer/main.go
@@ -1,5 +1,10 @@
 package main
 
+// If you absolutely NEEDED to import the local version of the package rather
+// than installing it as recommended, you can use the following import instead
+
+// import "../../pkg/buffers/_obj/jnwhiteh.net/buffers
+
 import "jnwhiteh.net/buffers"
 import "fmt"
 
